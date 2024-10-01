f6acacdba56a90dc3e5da8fd9b343b617ac786e6
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-01 11:39:57 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Navigation />
-    </BrowserRouter>
+    </Router>
   );
 }
 
