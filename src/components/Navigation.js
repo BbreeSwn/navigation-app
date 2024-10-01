cb682373275f0400e6de4124b787a443b5c58a1f
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-01 12:22:42 +0700

    feat : create show state

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,32 @@
 import { Link } from "react-router-dom";
+import{useState} from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaHome } from "react-icons/fa";
 // import { FaWindowClose } from "react-icons/fa";
 
 const Navigation = () => {
+    const [showMenu,setShowMenu] = useState(false)
+    const toggleMenu = () => setShowMenu(!showMenu)
   return (
     <aside>
       <div className="navbar">
         <div className="navbar-toggle">
           <Link to="#" className="menu-bar">
-            <GiHamburgerMenu/>
+            <GiHamburgerMenu onClick={toggleMenu} />
           </Link>
         </div>
       </div>
-      <nav className="nav-menu">
-<ul className="nav-menu-item">
-    <li className="menu-text"><Link to='#'><FaHome/><span>Home</span></Link></li>
-    <li className="menu-text">Home</li>
-    <li className="menu-text">Home</li>
-</ul>
+      <nav className={showMenu ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-item">
+          <li className="menu-text">
+            <Link to="#">
+              <FaHome />
+              <span>Home</span>
+            </Link>
+          </li>
+          <li className="menu-text">Home</li>
+          <li className="menu-text">Home</li>
+        </ul>
       </nav>
     </aside>
   );
