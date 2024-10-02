9d68f7dc967457b8f3f69571b2721e08ca01c6a2
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-02 15:49:51 +0700

    fix responsive

--- src/data/ProjectLink.js
+++ src/data/ProjectLink.js
@@ -1,52 +1,52 @@
 const projectLink = [
   {
     projectId: 1,
-    projectTitle: "Project 1",
+    projectTitle: "Task Management",
     link: "https://swn-task-management.netlify.app/",
   },
   {
     projectId: 2,
-    projectTitle: "Project 2",
+    projectTitle: "Blog App",
     link: "https://swn-blog-application.netlify.app/",
   },
   {
     projectId: 3,
-    projectTitle: "Project 3",
+    projectTitle: "Shopping App",
     link: "https://swn-shopping-application.netlify.app/",
   },
   {
     projectId: 4,
-    projectTitle: "Project 4",
+    projectTitle: "Accordian App",
     link: "https://swn-accordian-application.netlify.app/",
   },
   {
     projectId: 5,
-    projectTitle: "Project 5",
+    projectTitle: "Search Image",
     link: "https://swnsearchimageapp.netlify.app/",
   },
   {
     projectId: 6,
-    projectTitle: "Project 6",
+    projectTitle: "Form Validation",
     link: "https://swn-formvalidation.netlify.app/",
   },
   {
     projectId: 7,
-    projectTitle: "Project 7",
+    projectTitle: "Quiz App",
     link: "https://swn-quizapp.netlify.app/",
   },
   {
     projectId: 8,
-    projectTitle: "Project 8",
+    projectTitle: "Toggle mode",
     link: "https://swn-togglemode.netlify.app/",
   },
   {
     projectId: 9,
-    projectTitle: "Project 9",
+    projectTitle: "Acc2 App",
     link: "https://swn-acc-app.netlify.app/",
   },
   {
     projectId: 10,
-    projectTitle: "Project 10",
+    projectTitle: "Currency-Exchange",
     link: "https://swn-currency-exchange.netlify.app",
   },
 ];
